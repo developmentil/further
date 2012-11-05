8f30786d5adf1f2ec38025511d23ac03cc588c99
Moshe Simantov <ms@development.co.il>
2012-11-06 00:40:17 +0200

    tasks classes

--- index.js
+++ index.js
@@ -2,7 +2,10 @@ var further = module.exports;
 
 further.registery = require('./lib/registery');
 
+further.Serial = require('./lib/serial');
+further.Parallel = require('./lib/parallel');
 further.Controller = require('./lib/controller');
+
 further.setup = require('./lib/setup');
 further.requires = require('./lib/requires');
 further.require = require('./lib/require');
