1bbc66905c99c286777c0c5fc404357f36e108e5
owner <owner@MS-Workstation>
2013-07-21 02:13:32 +0300

    version 0.1.6 add "each" method for tasks containers expose Server add utils: unique, trim, escapeRexExp, htmlToText

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var further = module.exports;
 
 further.extend = require('./lib/extend');
+further.extend(further, require('./lib/utils'));
 
 further.reg = 
 further.registery = require('./lib/registery');
@@ -9,6 +10,7 @@ further.Serial = require('./lib/tasks/serial');
 further.Parallel = require('./lib/tasks/parallel');
 further.Controller = require('./lib/controller');
 further.Service = require('./lib/service');
+further.Server = require('./lib/server');
 further.HttpServer = require('./lib/httpServer');
 further.HttpsServer = require('./lib/httpsServer');
 
